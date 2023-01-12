e4cb4e0af9cc84747699dce0676950858251297a
Smiley98 <smileysofficialcinema@gmail.com>
2023-01-12 11:44:35 -0500

    Added console character input.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,15 @@
 
 int main()
 {
-	std::cout << "Welcome to intro to programming with your boy Smiley!" << std::endl;
+	std::cout << "Please enter any character" << std::endl;
+	char character;
+	std::cin >> character;
+	std::cout << "Initial character: " << character << std::endl;
+	// We can perform math operations to primitive (blue) datatypes
+	// using add (+), subtract (-), multiple (*) and divide (/).
+	// In this case, we change our character in correspondence with the ASCII table.
+	// https://www.asciitable.com/
+	character = character + 1;
+	std::cout << "Final character: " << character << std::endl;
 	return 0;
 }
