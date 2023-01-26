b73cf058b6bd9b95cab3dc316b12fc46a0b1fecd
Smiley98 <smileysofficialcinema@gmail.com>
2023-01-26 11:21:30 -0500

    Battle simulation implemented!

--- lab3.cpp
+++ lab3.cpp
@@ -7,24 +7,48 @@ int main()
 	cout << "Welcome to battle simulator 2023" << endl
 	     << "Preparing Player for battle..." << endl << endl;
 
+	// Input player health
 	int playerHealth = 0;
 	cout << "Please enter the player's health: " << endl;
 	cin >> playerHealth;
 
+	// Input player damage
 	int playerDamage = 0;
 	cout << "Please enter the player's damage: " << endl << endl;
 	cin >> playerDamage;
 
+	// Input monster health
 	int monsterHealth = 0;
 	cout << "Please enter the monster's health: " << endl;
 	cin >> monsterHealth;
 
+	// Input monster damage
 	int monsterDamage = 0;
 	cout << "Please enter the monster's damage: " << endl << endl;
 	cin >> monsterDamage;
 
+	// Monster attacks player
+	cout << "The monster fearsomely lunges at the player!" << endl;
+	playerHealth -= monsterDamage;
+	cout << "The player took " << monsterDamage << " damage." << endl
+		 << "Player health: " << playerHealth << "." << endl << endl;
+
+	// Player attacks monster
+	cout << "The player fights back in retaliation!" << endl;
+	monsterHealth -= playerDamage;
+	cout << "The monster took " << playerDamage << " damage." << endl
+		<< "Monster health: " << monsterHealth << "." << endl;
+
 	return 0;
 }
+
+// TOO MUCH COMMENTING:
+//int playerHealth = 0;									// declare player health variable
+//cout << "Please enter the player's health: " << endl; // prompt user to enter player's health
+//cin >> playerHealth;									// receive user player's health input
+
+// "-=" is the exact same as a = a - b.
+//playerHealth = playerHealth - monsterDamage;
 
 // We don't want to use single letters for variable names,
 // we want to make our variable names as informative as possible!
