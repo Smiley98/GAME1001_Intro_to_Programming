b09646a1d0509c41bee64aef9571d7ae9b001d9b
Smiley98 <smileysofficialcinema@gmail.com>
2023-01-26 12:40:08 -0500

    Added lab3 again starter file

--- lab3.cpp
+++ lab3.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/*#include <iostream>
 
 using namespace std;
 
@@ -55,7 +55,7 @@ int main()
 	}
 
 	return 0;
-}
+}*/
 
 // booleans are true/false types, for example 
 // bool condition = true;
