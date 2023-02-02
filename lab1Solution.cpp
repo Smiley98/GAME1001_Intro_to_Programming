4344a95aee32add020a07fb97905b9571aa2948f
Smiley98 <smileysofficialcinema@gmail.com>
2023-02-02 12:34:20 -0500

    Removed duplicate labs

---
+++ lab1Solution.cpp
@@ -0,0 +1,34 @@
+/*
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+	int muffinCount = 0;
+	cout << "How many muffins were sold?" << endl;
+	cin >> muffinCount;
+
+	float muffinPrice = 0.0f;
+	cout << "What is the muffin price?" << endl;
+	cin >> muffinPrice;
+
+	int donutCount = 0;
+	cout << "How many donut were sold?" << endl;
+	cin >> donutCount;
+
+	float donutPrice = 0.0f;
+	cout << "What is the donut price?" << endl;
+	cin >> donutPrice;
+
+	float muffinSales = muffinCount * muffinPrice;
+	float donutSales = donutCount * donutPrice;
+	cout << "Total items sold: " << muffinCount + donutCount << endl;
+	cout << "Muffin sales: " << muffinSales << endl;
+	cout << "Donut sales: " << donutSales << endl;
+	cout << "Total sales: " << muffinSales + donutSales << endl;
+
+	return 0;
+}//*/
