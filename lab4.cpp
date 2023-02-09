806b138c0abf10e9edbf7038d31d8cea3fce5b4f
Smiley98 <smileysofficialcinema@gmail.com>
2023-02-09 11:17:34 -0500

    Finished lab 4 -- upgraded battle simulation!

--- lab4.cpp
+++ lab4.cpp
@@ -36,18 +36,21 @@ int main()
 
 		int testPlayerHealth = playerHealth;
 		int testMonsterHealth = monsterHealth;
+		int minTurnCount = 0;
 		cout << "Calculating battle simulation..." << endl;
-		for (int turn = 0; turn < turnCount; turn++)
+
+		while (testPlayerHealth > 0 || testMonsterHealth > 0)
 		{
+			minTurnCount++;
 			testPlayerHealth -= monsterDamage;
 			testMonsterHealth -= playerDamage;
 		}
 		
 		// Test if there were enough turns for either the player or the monster to emerge victious!
-		if (testPlayerHealth <= 0 || testMonsterHealth <= 0)
+		if (minTurnCount <= turnCount)
 			break;
 		else
-			cout << "Not enough turns, please enter a greater number of turns" << endl;
+			cout << "You need " << minTurnCount - turnCount << " more turns for a battle to the death!" << endl;
 	}
 
 	cout << "PREPARE FOR BATTLE!!!" << endl;
@@ -65,21 +68,6 @@ int main()
 		cout << "The monster took " << playerDamage << " damage." << endl
 			<< "Monster health: " << monsterHealth << "." << endl;
 	}
-
-	//while (playerHealth > 0 && monsterHealth > 0)
-	//{
-	//	// Monster attacks player
-	//	cout << endl << "The monster fearsomely lunges at the player!" << endl;
-	//	playerHealth -= monsterDamage;
-	//	cout << "The player took " << monsterDamage << " damage." << endl
-	//		<< "Player health: " << playerHealth << "." << endl;
-	//
-	//	// Player attacks monster
-	//	cout << endl << "The player fights back in retaliation!" << endl;
-	//	monsterHealth -= playerDamage;
-	//	cout << "The monster took " << playerDamage << " damage." << endl
-	//		<< "Monster health: " << monsterHealth << "." << endl;
-	//}
 
 	// Win condition (player or monster health must be less than or equal to zero)
 	if (playerHealth <= 0 && monsterHealth <= 0)
