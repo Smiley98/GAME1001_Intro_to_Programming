84ef9a8077d3594dd9383b91ef1537f3f6b6209d
Smiley98 <smileysofficialcinema@gmail.com>
2023-02-23 09:52:59 -0500

    Added file name input from console

--- lab6.cpp
+++ lab6.cpp
@@ -1,22 +1,28 @@
 #include <fstream>
 #include <iostream>
+#include <string>
 
 // File input/output is just like console input/output!
 //std::cout << wins;
 
 int main()
 {
-	std::ofstream outFile("test.txt");
+	std::cout << "Please enter a file name." << std::endl;
+	std::string fileName;
+	std::getline(std::cin, fileName);
+	fileName += ".txt";
+
+	std::ofstream outFile(fileName);
 	int wins = 4, loses = 5, draws = 6;
 	outFile << wins << " " << loses << " " << draws << " ";
 	outFile.close();
 
-	std::ifstream inFile("test.txt");
+	std::ifstream inFile(fileName);
 	wins = -1; loses = -1; draws = -1;
 	inFile >> wins >> loses >> draws;
 	inFile.close();
 
-	outFile.open("test.txt", std::ios::out | std::ios::trunc);
+	outFile.open(fileName, std::ios::out | std::ios::trunc);
 
 	return 0;
 }
